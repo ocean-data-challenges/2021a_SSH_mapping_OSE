a53432d7ef81272dab6854108452a29d4d35e0eb
Maxime Ballarotta <36267733+mballaro@users.noreply.github.com>
2021-09-14 11:41:11 +0200

    Update mod_plot.py

--- src/mod_plot.py
+++ src/mod_plot.py
@@ -72,9 +72,11 @@ def plot_psd_score(filename):
 def plot_spatial_statistics(filename):
     
     ds = xr.open_dataset(filename, group='diff')
-
-    figure = ds['rmse'].hvplot.image(x='lon', y='lat', z='rmse', clabel='RMSE [m]', cmap='Reds', coastline=True)
-    
+    try:
+        figure = ds['rmse'].hvplot.image(x='lon', y='lat', z='rmse', clabel='RMSE [m]', cmap='Reds', coastline=True)
+    except KeyError:
+        figure = ds['rmse'].hvplot.image(x='lon', y='lat', z='rmse', clabel='RMSE [m]', cmap='Reds')
+        
     return figure
 
 
