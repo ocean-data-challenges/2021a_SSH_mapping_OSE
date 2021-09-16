be0db1f63f5738a00ac714dcb9f03b3ccbf79ba0
Maxime Ballarotta <36267733+mballaro@users.noreply.github.com>
2021-09-16 13:53:54 +0200

    remove coastline=True due to error on binder demo

--- src/mod_plot.py
+++ src/mod_plot.py
@@ -72,10 +72,12 @@ def plot_psd_score(filename):
 def plot_spatial_statistics(filename):
     
     ds = xr.open_dataset(filename, group='diff')
-    try:
-        figure = ds['rmse'].hvplot.image(x='lon', y='lat', z='rmse', clabel='RMSE [m]', cmap='Reds', coastline=True)
-    except KeyError:
-        figure = ds['rmse'].hvplot.image(x='lon', y='lat', z='rmse', clabel='RMSE [m]', cmap='Reds')
+#     try:
+#         figure = ds['rmse'].hvplot.image(x='lon', y='lat', z='rmse', clabel='RMSE [m]', cmap='Reds', coastline=True)
+#     except KeyError:
+#         figure = ds['rmse'].hvplot.image(x='lon', y='lat', z='rmse', clabel='RMSE [m]', cmap='Reds')
+
+    ds['rmse'].hvplot.image(x='lon', y='lat', z='rmse', clabel='RMSE [m]', cmap='Reds')
         
     return figure
 
